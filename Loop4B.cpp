1358a703a6f2403261606da9f0638b303d4da9e6
angelinawardani-source <angelinawardani@student.ub.ac.id>
2025-09-26 18:06:23 +0700

    Add files via upload

---
+++ Loop4B.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int nilai = 2;              
+    while (nilai <= 10) {        
+        cout << nilai << endl;   
+        nilai += 2;              
+    }
+    return 0;
+}
